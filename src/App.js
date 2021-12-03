d528883e2785bf7755143dcd7b83c970f40e653f
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-12-03 17:12:32 -0300

    configurando app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import React, { Fragment } from "react";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import '../src/style/style.css'
+
 const App = (_) => {
-  return <h1>Teste</h1>;
+  return (
+    <Fragment>
+      <Header />
+      <Dashboard />
+    </Fragment>
+  );
 };
 
 export default App;
