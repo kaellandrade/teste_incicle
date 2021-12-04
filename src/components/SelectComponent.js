3f3488da4d952e507d3263df6fc944b3092a4126
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-12-04 16:17:19 -0300

    Update SelectComponent.js

--- src/components/SelectComponent.js
+++ src/components/SelectComponent.js
@@ -2,19 +2,21 @@ import React from "react";
 
 /**
  * Renderiza um componente select
- * Recebe um array de objetos e retorna uma tag option.
+ * Recebe um array de objetos e retorna uma tags options.
  *
  */
-function SelectComponent({ name, id, options, className }) {
+const SelectComponent = ({ name, id, options, className }) => {
   return (
     <div className={className}>
       <select className="form-select form-select-md" name={name} id={id}>
-        {options.map(({ value, text }) => (
-          <option value={value}>{text}</option>
+        {options.map(({ value, text }, index) => (
+          <option key={index} value={value}>
+            {text}
+          </option>
         ))}
       </select>
     </div>
   );
-}
+};
 
 export default SelectComponent;
