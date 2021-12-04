7430ba6865adaeb6174c780ebcbe7299c1746f8e
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-12-03 22:11:34 -0300

    estruturando componetne card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Container from "../layouts/Container";
+import Column from "../layouts/Column";
+import Row from "../layouts/Row";
 
 /**
  * Componente que representa um Card para ser listado no Dashboard
@@ -7,16 +10,23 @@ const Card = ({ srcImg, alt, title, type, date, description }) => {
   return (
     <div className="div-card">
       <figure>
-        <div className="div-img">
-          <img className="img-fluid img-card" alt={alt} src={srcImg} />
-        </div>
-
-        <div className="content">
-          <h3 className="title">{title}</h3>
-          <span>{type}</span>
-          <span>{date}</span>
-          <figcaption>{description}</figcaption>
-        </div>
+        <Container className="container">
+          <Row>
+            <Column classStyle="col-4 col-md-2">
+              <div className="div-img">
+                <img className="img-fluid img-card" alt={alt} src={srcImg} />
+              </div>
+            </Column>
+            <Column classStyle="col-8 col-md-10">
+              <div className="content">
+                <h3 className="title">{title}</h3>
+                <span className={type}>{type.toUpperCase()} </span>
+                <span>{date}</span>
+                <figcaption>{description}</figcaption>
+              </div>
+            </Column>
+          </Row>
+        </Container>
       </figure>
     </div>
   );
