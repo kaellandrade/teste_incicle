b1759967341ad94608c79b5e769e3f9ead18ac8b
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-12-03 22:12:18 -0300

    responsividade

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,30 +1,50 @@
 import React, { Fragment } from "react";
 import Card from "../components/Card";
+import Container from "../layouts/Container";
+import Column from "../layouts/Column";
+import Row from "../layouts/Row";
+import CardInfo from "../components/CardInfo";
+
 /**
  * Tela principal listagem dos dados.
  */
 const Dashboard = (_) => {
   return (
     <Fragment>
-      <section className='page-events'>
-        <div className="events">
-          <Card
-            title="Geral dispensado para curtir o feriado"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidindut..."
-            type="release"
-            srcImg="https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg"
-            date="15 OUT"
-          />
-          <Card
-            title="Geral dispensado para curtir o feriado"
-            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidindut..."
-            type="release"
-            srcImg="https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg"
-            date="15 OUT"
-          />
-        </div>
-
-        <div className="side-bar"></div>
+      <section className="page-events pt-4">
+        <Container Classname="container">
+          <Row>
+            <Column classStyle="col-12 col-lg-9">
+              <div className="events">
+                <Card
+                  title="Geral dispensado para curtir o feriado"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidindut..."
+                  type="release"
+                  srcImg="https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg"
+                  date="15 OUT"
+                />
+                <Card
+                  title="Geral dispensado para curtir o feriado"
+                  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incidindut..."
+                  type="event"
+                  srcImg="https://cdn.pixabay.com/photo/2017/01/08/13/58/cube-1963036__340.jpg"
+                  date="15 OUT"
+                />
+              </div>
+            </Column>
+            <Column classStyle="col-12 col-lg-3">
+              <div className="side-bar">
+                <CardInfo title="Endormarketing">
+                  Endomarketing está relacionado às ações de treinamento ou
+                  qualificação dos colaboradoresda empresa visndo um melhor
+                  serviço para o cliente. Marketing interno, devido ao nome, é
+                  usualmente confundido com Endomarketing mesmo sendo conceitos
+                  diferentes.
+                </CardInfo>
+              </div>
+            </Column>
+          </Row>
+        </Container>
       </section>
     </Fragment>
   );
