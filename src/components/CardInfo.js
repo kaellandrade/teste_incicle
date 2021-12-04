06d8836dbd540cdc476f127abd5cfc348ccdd7b2
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-12-03 22:11:40 -0300

    componente card info

---
+++ src/components/CardInfo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CardInfo = ({ children, title }) => {
+  return (
+    <div className="card-info">
+      <h5>{title}</h5>
+      <p className="text-info">{children}</p>
+      <button type='button' className='btn btn-outline-secondary'>DISPENSAR</button>
+    </div>
+  );
+};
+
+export default CardInfo;
